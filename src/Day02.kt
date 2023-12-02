cecd116a29924c78f90e4bddd69593aa8a034198
Ga Jun Young <ga.young@personio.de>
2023-12-02 17:51:57 +0000

    [Day02] Refactor

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,65 +1,48 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var sum = 0
 
-        input.forEach {
-            val game =
-                it.substringBefore(":")
-                    .substringAfter("Game ")
-                    .toInt()
-            val colors =
-                it.substringAfter(": ")
-                    .replace(";", ",")
-                    .split(",")
-            val c = mutableMapOf("blue" to 14, "green" to 13, "red" to 12)
-
-            var b = true
-            colors.forEach { p ->
-                val pair =
-                    p.trim()
-                        .partition { s -> s.isDigit() }
-
-                if (pair.first.toInt() > c[pair.second.trim()]!!) {
-                    b = false
-                    return@forEach
+    fun String.toColorAndCountPairs() =
+            trim()
+                .replace(";", ",") // sets do not matter, we only care about the Num - Color.
+                .split(",")
+                .map {
+                    val (count, color) = it.trim().split(" ")
+                    color to count.toInt()
                 }
-            }
 
-            if (b) {
-                sum += game
+    fun String.possibleGame(): Int {
+        val (gameId, rawSets) = split(":")
+        val id = gameId.substringAfter("Game ").toInt()
+
+        val colorCountPairs = rawSets.toColorAndCountPairs()
+        val isWithinPossibilities = colorCountPairs.all { (color, count) ->
+            when(color) {
+                "red" -> count <= 12
+                "green" -> count <= 13
+                "blue" -> count <= 14
+                else -> false
             }
         }
-        return sum
+
+        return if(isWithinPossibilities) id else 0
     }
 
-    fun part2(input: List<String>): Int {
-        var sum = 0
+    fun String.powerSet(): Int {
+        val rawSets = substringAfter(": ")
+        val colorCountPairs = rawSets.toColorAndCountPairs()
 
-        input.forEach {
-            val colors =
-                it.substringAfter(": ")
-                    .replace(";", ",")
-                    .split(",")
-            val c = mutableMapOf("blue" to 0, "green" to 0, "red" to 0)
-
-            colors.forEach { p ->
-                val pair =
-                    p.trim()
-                        .partition { s -> s.isDigit() }
-
-                if (pair.first.toInt() > c[pair.second.trim()]!!) {
-                    c[pair.second.trim()] = pair.first.toInt()
-                }
-            }
-
-            val p = c["green"]!! * c["blue"]!! * c["red"]!!
-            sum += p
+        val highestCubes = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+        colorCountPairs.forEach { (color, count) ->
+            highestCubes[color] = maxOf(highestCubes[color]!!.toInt(), count)
         }
-        return sum
+
+        return highestCubes.values.reduce { acc, i -> acc * i }
     }
+
+    fun part1(input: List<String>) = input.sumOf { it.possibleGame() }
+
+    fun part2(input: List<String>): Int = input.sumOf { it.powerSet() }
 
     val input = readInput("data/DAY02")
-    part1(input).println()
-    println("Part 2")
-    part2(input).println()
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
