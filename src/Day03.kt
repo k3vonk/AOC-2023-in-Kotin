68a35fa6c016dd969f24c14c31ac054a7d2f51b1
Ga Jun Young <ga.young@personio.de>
2023-12-03 12:51:59 +0000

    [Day03] Kinda a refactor 🤷

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,77 +1,99 @@
 fun main() {
-    val directions = arrayOf(
-        Pair(-1, -1), Pair(-1, 0), Pair(-1, 1),
-        Pair(0, -1), Pair(0, 1),
-        Pair(1, -1), Pair(1, 0), Pair(1, 1)
-    )
+    val directions =
+        arrayOf(
+            Pair(-1, -1),
+            Pair(-1, 0),
+            Pair(-1, 1),
+            Pair(0, -1),
+            Pair(0, 1),
+            Pair(1, -1),
+            Pair(1, 0),
+            Pair(1, 1),
+        )
 
-    val foundMatrix = mutableMapOf<String, List<Int>>()
+    val foundStars = mutableMapOf<String, List<Int>>()
+
+    fun adjacentSymbolLocation(
+        i: Int,
+        j: Int,
+        matrix: List<List<Char>>,
+    ): Pair<Int, Int>? {
+        directions.forEach { direction ->
+            val newI = i + direction.first
+            val newJ = j + direction.second
+
+            // out of bounds
+            if (newI < 0 || newJ < 0 || newI >= matrix.size || newJ >= matrix[i].size) {
+                return@forEach
+            }
+
+            val searchValue = matrix[newI][newJ]
+            if (searchValue != '.' && !searchValue.isDigit()) {
+                return Pair(newI, newJ)
+            }
+        }
+        return null
+    }
+
+    fun updateFoundStars(
+        foundStarLocation: String,
+        numberString: Int,
+    ) {
+        if (foundStarLocation.isNotBlank()) {
+            foundStars[foundStarLocation] =
+                (foundStars[foundStarLocation] ?: emptyList()) + listOf(numberString)
+        }
+    }
+
+    fun getNumberIfAdjacentToSymbol(
+        isAdjacentToSymbol: Boolean,
+        foundStarLocation: String,
+        numberString: String,
+    ) = if (isAdjacentToSymbol) {
+        val number = numberString.toInt()
+        updateFoundStars(foundStarLocation, number)
+        number
+    } else {
+        0
+    }
+
     fun part1(matrix: List<List<Char>>): Int {
         var sum = 0
-        var numberString = ""
-
-        println("size: [${matrix.size}][${matrix[0].size}]")
         matrix.forEachIndexed { i, _ ->
-            var searched = false
-            var isStar = ""
-            matrix[i].forEachIndexed { j, jChar ->
-                if(jChar == '.' || !jChar.isDigit()) {
-                    if (searched) {
-                        sum += numberString.toInt()
-                        println(numberString)
-                        searched = false
+            var isAdjacentToSymbol = false
+            var numberString = ""
+            var foundStarLocation = ""
 
-                        if(isStar.isNotBlank()) {
-                            foundMatrix[isStar] = (foundMatrix[isStar] ?: emptyList()) + listOf(numberString.toInt())
-                        }
-                        isStar = ""
-                    }
+            matrix[i].forEachIndexed { j, char ->
+                if (!char.isDigit()) {
+                    sum += getNumberIfAdjacentToSymbol(isAdjacentToSymbol, foundStarLocation, numberString)
+                    isAdjacentToSymbol = false
                     numberString = ""
+                    foundStarLocation = ""
                     return@forEachIndexed
                 }
-                numberString += jChar
-                directions.forEach { direction ->
-                    val newI = i + direction.first
-                    val newJ = j + direction.second
 
-                    if(newI < 0 || newJ < 0) {
-                        return@forEach
-                    }
-                    if (newI < matrix.size && newJ < matrix[i].size) {
-                        val searchValue = matrix[newI][newJ]
-                        if(searchValue != '.' && !searchValue.isDigit()) {
-                            searched = true
-                            if(searchValue == '*') {
-                                isStar = "[$newI][$newJ]"
-                            }
-                        }
+                numberString += char
+                val location = adjacentSymbolLocation(i, j, matrix)
+                if (location != null) {
+                    isAdjacentToSymbol = true
+                    if (matrix[location.first][location.second] == '*') {
+                        foundStarLocation = "[${location.first}][${location.second}]"
                     }
                 }
             }
 
-            if(searched) {
-                sum += numberString.toInt()
-                searched = false
-                if(isStar.isNotBlank()) {
-                    foundMatrix[isStar] = (foundMatrix[isStar] ?: emptyList()) + listOf(numberString.toInt())
-                }
-                isStar = ""
-                numberString = ""
-            }
-        }
-
-        return sum
-    }
-
-    fun part2(): Int {
-        var sum = 0
-        foundMatrix.forEach { key, value ->
-            if(value.size > 1) {
-                sum += value.reduce { acc, s -> acc * s }
-            }
+            sum += getNumberIfAdjacentToSymbol(isAdjacentToSymbol, foundStarLocation, numberString)
         }
         return sum
     }
+
+    fun List<Int>.getProduct() = reduce { acc, s -> acc * s }
+
+    fun part2() =
+        foundStars
+            .map { (_, values) -> if (values.size > 1) values.getProduct() else 0 }
+            .sumOf { it }
 
     val input = readInput("DAY03")
     val matrix: List<List<Char>> = input.map { line -> line.map { it } }
